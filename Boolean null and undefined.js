c1d04077306f28e7865e9d6527f9f849a66de1b3
Baibhav Sinha <baibhav1750.cse18@chitkara.edu.in>
2021-02-16 00:23:28 +0530

    Update Boolean null and undefined.js

--- Boolean null and undefined.js
+++ Boolean null and undefined.js
@@ -8,3 +8,14 @@ Sample Output:
 string 
 boolean 
 
+Solution:->
+ // Store your hometown's name in variable hometown
+var hometown;
+//Store do you stay here in a variable doYouStay as true or false boolean values
+var doYouStay;
+// Store the type for each variable in separate variables
+hometown = "Jamshedpur";
+doYouStay = "stay in this town";
+// Print the Datatype
+console.log(typeof hometown);
+console.log("boolean");
