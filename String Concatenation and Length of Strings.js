0d8a89102317be6aaa2dfaac52ba8753abb9a0aa
Baibhav Sinha <baibhav1750.cse18@chitkara.edu.in>
2021-02-15 22:03:51 +0530

    Update String Concatenation and Length of Strings.js

--- String Concatenation and Length of Strings.js
+++ String Concatenation and Length of Strings.js
@@ -1,1 +1,11 @@
+Description
+You have been given two strings stringOne = ‘UpGrad’ and stringTwo = ‘Education’. You have to output a new string “UpGradEducation”. You also have to output the number of alphabets in the new string.
+ 
+ 
+Sample
+Output: 
+UpGradEducation 
+15 
 
+Solution :->
+  
