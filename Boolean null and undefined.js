7ae5df9417d0936e840170cb72b71524d840e7d3
Baibhav Sinha <baibhav1750.cse18@chitkara.edu.in>
2021-02-16 00:22:37 +0530

    Update Boolean null and undefined.js

--- Boolean null and undefined.js
+++ Boolean null and undefined.js
@@ -1,1 +1,10 @@
+DataTypeOf
+Description
+You have to store your hometown's name in variable hometown. Also you have to declare and initialize a variable doYouStay which stores either true or false. True corresponds to the fact that you currently stay in this town and false states that currently you do not stay in this town .
+ 
+You have to print their data type on the console.
+ 
+Sample Output:
+string 
+boolean 
 
