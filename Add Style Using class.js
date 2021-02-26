c79d2dcf4df75e190e19a6b89a0b530fe31cfce8
Baibhav Sinha <baibhav1750.cse18@chitkara.edu.in>
2021-02-26 23:58:22 +0530

    Update Add Style Using class.js

--- Add Style Using class.js
+++ Add Style Using class.js
@@ -1,1 +1,41 @@
+<!DOCTYPE html>
+<html>
+<title>Add Style.JS</title>
+<style>
+.class1 {
+    background-color: orange;
+    font-size: 30px;
+    color: white;
+}
+.class2 {
+    padding: 20px;
+    border: 5px solid red;
+}
+</style>
+<script src="https://www.w3schools.com/lib/w3.js"></script>
+<body>
+
+<h2>Testing</h2>
+
+<p>Click the button to add class='class1 class2' to id='London'.</p>
+
+<button onclick="w3.addClass('#London','class1 class2')">Add Classes</button>
+
+<div id="London" class="city">
+  <h2>London</h2>
+  <p>London is the capital city of England.</p>
+</div>
+
+<div id="Paris" class="city">
+  <h2>Paris</h2>
+  <p>Paris is the capital of France.</p> 
+</div>
+
+<div id="Tokyo" class="city">
+  <h2>Tokyo</h2>
+  <p>Tokyo is the capital of Japan.</p>
+</div>
+
+</body>
+</html>
 
