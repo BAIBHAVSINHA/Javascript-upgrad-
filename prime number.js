d71e7ab29aaaea8631115cf4b527f26730575d0b
Baibhav Sinha <baibhav1750.cse18@chitkara.edu.in>
2021-02-26 14:49:52 +0530

    Update prime number.js

--- prime number.js
+++ prime number.js
@@ -1,1 +1,15 @@
-
+var fno = prompt('Enter a no: ');
+    var flag = 1;
+for(var i=2;i*i<=fno;i++)
+   {
+      if(fno%i == 0)
+      {
+         flag = 0;
+         break;
+      }
+   }
+if(flag == 1)
+{
+  console.log("prime");}
+else{
+  console.log("not-prime");}
