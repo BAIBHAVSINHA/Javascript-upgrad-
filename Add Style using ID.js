1b77e975eceee76ca1ddaac35e2906ca57e30fbf
Baibhav Sinha <baibhav1750.cse18@chitkara.edu.in>
2021-02-26 23:57:21 +0530

    Update Add Style using ID.js

--- Add Style using ID.js
+++ Add Style using ID.js
@@ -1,1 +1,32 @@
+<!DOCTYPE html>
+<html>
+<title>Add Style JS</title>
+<script src="https://www.w3schools.com/lib/w3.js"></script>
+<body>
+
+<h2>Testing</h2>
+
+<p>Click the button to add a red background color to id="London".</p>
+
+<p>
+<button onclick="w3.addStyle('#London','background-color','red')">Add Style</button>
+</p>
+
+<div id="London" class="city">
+  <h2>London</h2>
+  <p>London is the capital city of England.</p>
+</div>
+
+<div id="Paris" class="city">
+  <h2>Paris</h2>
+  <p>Paris is the capital of France.</p> 
+</div>
+
+<div id="Tokyo" class="city">
+  <h2>Tokyo</h2>
+  <p>Tokyo is the capital of Japan.</p>
+</div>
+
+</body>
+</html>
 
